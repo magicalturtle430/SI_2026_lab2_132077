be48f2bb0d1f6e676ba57ab2bf1cd3910d8e6013
magicalturtle430 <144983455+magicalturtle430@users.noreply.github.com>
2026-05-17 22:30:06 +0200

    Create initial test for Library class
    Add a test case to verify Library initialization.

---
+++ src/test/java/SI2026Lab2Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SI2026Lab2Test {
+    
+    @Test
+    public void testInitialization() {
+        // Овде ќе се допишуваат тест случаите во следните чекори
+        Library library = new Library();
+        assertNotNull(library);
+    }
+}
