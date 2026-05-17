47915dfb68a959ccb1c40df80fa98bf98da9c6bc
magicalturtle430 <144983455+magicalturtle430@users.noreply.github.com>
2026-05-17 22:29:04 +0200

    Create SI2026Lab2Main.java

---
+++ src/main/java/SI2026Lab2Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// ... (тука идат класите Book и Library кои ги пративте претходно) ...
+
+public class SI2026Lab2Main { // Променето во Lab2
+    public static void main(String[] args) {
+        // Главната метода на апликацијата
+    }
+}
